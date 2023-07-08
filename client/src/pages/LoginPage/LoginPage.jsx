343d3218e1515cd634292d87906265a14934d44a
AnasAttar10 <anasattar2010@gmail.com>
2023-07-08 14:14:30 +0300

    navigate between login and register pages

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Login from "../../components/LoginForm/Login";
 import RegistryContainer from "../../components/RegistryContainer/RegistryContainer";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <RegistryContainer>
       <Login />
+      <p style={{ margin: "auto" }}>
+        if you don't have an account , <Link to={"/register"}>Register</Link>
+      </p>
     </RegistryContainer>
   );
 };
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -2,12 +2,16 @@ import { Box, Grid } from "@mui/material";
 import React from "react";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import RegistryContainer from "../../components/RegistryContainer/RegistryContainer";
+import { Link } from "react-router-dom";
 // import "./register.css";
 
 const RegisterPage = () => {
   return (
     <RegistryContainer>
       <RegisterForm />
+      <p style={{ margin: "auto" }}>
+        if you have an account , <Link to={"/login"}>login</Link>
+      </p>
     </RegistryContainer>
   );
 };
